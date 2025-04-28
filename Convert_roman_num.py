e037148cff5a600a823188fd762992a816314abb
Johnalug99 <user.johnalug99@gmail.com>
2025-04-28 21:22:45 +0100

    Improved with streamlit methods

--- Convert_roman_num.py
+++ Convert_roman_num.py
@@ -1,21 +1,26 @@
 import streamlit as st
+
 def roman_converter():
     st.title('Roman Numeral Converter')
-    roman_num = {'D':1000, 'M':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
-    while True:
-     question = st.text_input('What roman numeral are you converting? :')
-     question1 = question.upper()
-     score = []
-     for i in question1:
-        score.append(roman_num[i])
-        if score[0] < score[1]:
-            number = score[1] - score[0]
-            print('The number is{:3d}'.format(number))
-        else:
-            number = sum(score)
-            print('The number is {:3d}'.format(number))
-        another = input('Do you want to play again? (Yes or No)').lower()  
-        if another != 'yes':
-        ('Goodbye')
-        break
+
+    roman_num = {'D': 1000, 'M': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+
+    question = st.text_input('Enter the Roman numeral you want to convert:')
+    
+    if question:
+        question1 = question.upper()
+        score = []
+        try:
+            for i in question1:
+                score.append(roman_num[i])
+            
+            if len(score) >= 2 and score[0] < score[1]:
+                number = score[1] - score[0]
+            else:
+                number = sum(score)
+
+            st.success(f'The number is: {number}')
+        except KeyError:
+            st.error("Invalid Roman numeral. Please enter valid characters (D, M, C, L, X, V, I).")
+
 roman_converter()
